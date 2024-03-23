ef8e681d732959f5f0541d8a4b0df1c9da336df2
novD07 <hoxuandai02@gmail.com>
2024-03-24 00:04:35 +0700

    init project

---
+++ src/main/java/com/example/comicwebbe/ComicWebBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.comicwebbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicWebBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComicWebBeApplication.class, args);
+    }
+
+}
