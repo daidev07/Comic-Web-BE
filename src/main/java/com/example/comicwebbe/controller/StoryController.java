5b38569032d1b05578a003063cd758a79d274320
minhtuan12c9 <hophanminhtuan2619@gmail.com>
2024-04-03 01:22:13 +0700

    build logic

--- src/main/java/com/example/comicwebbe/controller/StoryController.java
+++ src/main/java/com/example/comicwebbe/controller/StoryController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@RequestMapping("api/admin")
+@RequestMapping("api/home")
 public class StoryController {
     @Autowired
     private StoryService storyService;
